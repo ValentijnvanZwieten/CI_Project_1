f5c8e657016f244e5828a81bd0e14d4a0b3cbf1d
Valentijn van Zwieten <V.L.vanzwieten@students.uu.nl>
2019-05-09 10:45:59 +0200

    Improvement search

--- sudoku/sudoku/Program.cs
+++ sudoku/sudoku/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace sudoku
 {
@@ -13,15 +14,18 @@ namespace sudoku
 
     class Sudoku
     {
-        // structuur
+        // sudoku
         int[] blocks = new int[9 * 9];
-        int mask = 0;
+        bool[] mask = new bool[9 * 9];
         int score;
+
+        Random rnd = new Random();
 
         // initialiseer de sudoku en pas een algoritme toe
         public Sudoku(string alg)
         {
             Parse();
+            EvalAll();
 
             if (alg == "ILS") IteratedLocalSearch();
             else if (alg == "SAS") SimulatedAnnealingSearch();
@@ -30,9 +34,14 @@ namespace sudoku
 
         // lees de sudoku uit een file
         // todo meerdere sudokus
+        // todo verschillende groottes
         private void Parse()
         {
+            // todo omgaan met lege regels
             Console.ReadLine();
+
+            int index;
+
             // loop door de individuele elementen van de puzzel
             for (int y = 0; y < 9; y++)
             {
@@ -44,8 +53,9 @@ namespace sudoku
                     if (c != 0)
                     {
                         // ... sla het dan op
-                        blocks[x + y * 9] = c;
-                        mask |= (1 << x + y * 9);
+                        index = x + y * 9;
+                        blocks[index] = c;
+                        mask[index] = true;
                     }
                 }
                 // ga naar de volgende regel
@@ -53,6 +63,7 @@ namespace sudoku
             }
         }
 
+        // evalueer de hele sudoku
         private void EvalAll()
         {
             score = 0;
@@ -62,6 +73,7 @@ namespace sudoku
                 score += Eval(x, x);
             }
         }
+        // evalueer een specifieke rij/kolom combinatie
         private int Eval(int row, int column)
         {
             int flag = 0;
@@ -93,8 +105,11 @@ namespace sudoku
         }
 
         // wissel de waardes van twee coordinaten
-        private void Swap(int x1, int y1, int x2, int y2)
+        private int Swap(int x1, int y1, int x2, int y2)
         {
+            // voorkom dat vaste waardes gewisseld worden
+            if (Value(x1, y1, mask) || Value(x2, y2, mask)) throw new ArgumentException("Swapped value(s) is/are static");
+
             // bewaar de originele score
             int oldscore = Eval(x1, y1) + Eval(x2, y2);
 
@@ -103,18 +118,57 @@ namespace sudoku
             blocks[index1] = blocks[index2];
             blocks[index2] = value1;
 
-            // update de score
-            score += oldscore - (Eval(x1, y1) + Eval(x2, y2));
+            // geef het verschil in scores terug
+            return (Eval(x1, y1) + Eval(x2, y2)) - oldscore;
         }
         // lees de waarde van een coordinaat
-        private int Value(int x, int y)
+        private T Value<T>(int x, int y, T[] a)
         {
-            return blocks[x + y * 9];
+            return a[x + y * 9];
         }
 
         private void IteratedLocalSearch()
         {
-            // todo implementeren
+            // bepaal de linkerboven-coordinaat van het block dat je gaat onderzoeken
+            int block = rnd.Next(9);
+            int offsety = (block - 1) / 3 * 3;
+            int offsetx = (block - 1) % 3 * 3;
+            List<Tuple<int, int>> coordinates = new List<Tuple<int, int>>();
+
+            // verzamel alle coordinaten die verwisseld kunnen worden
+            for (int y = offsety; y < offsety + 3; y++)
+            {
+                for (int x = offsetx; x < offsetx + 3; x++)
+                {
+                    if (!Value(x, y, mask)) coordinates.Add(new Tuple<int, int>(x, y));
+                }
+            }
+
+            // verwissel deze coordinaten en bepaal welke keuze het beste is
+            Tuple<int, int> currentcoordinate, bestcoordinate1, bestcoordinate2;
+            int currentscore, lowestscore = 81;
+            while (coordinates.Count > 0)
+            {
+                currentcoordinate = coordinates[0];
+                coordinates.RemoveAt(0);
+
+                foreach (Tuple<int, int> coordinate2 in coordinates)
+                {
+                    currentscore = Swap(currentcoordinate.Item1, currentcoordinate.Item2, coordinate2.Item1, coordinate2.Item2);
+
+                    if (currentscore < lowestscore)
+                    {
+                        lowestscore = currentscore;
+                        bestcoordinate1 = currentcoordinate;
+                        bestcoordinate2 = coordinate2;
+                    }
+
+                    Swap(coordinate2.Item1, coordinate2.Item2, currentcoordinate.Item1, currentcoordinate.Item2);
+                }
+            }
+
+            if (lowestscore < 0) Swap(bestcoordinate1.Item1, bestcoordinate1.Item2, bestcoordinate2.Item1, bestcoordinate2.Item2);
+
         }
         private void SimulatedAnnealingSearch()
         {
